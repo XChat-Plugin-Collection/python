cca5bd3622e562762fc7da428fac732d2fb4d47e
Avail <kanade7331@gmail.com>
2014-04-15 11:36:18 +0200

    Converts text to fullwidth - /atou

---
+++ atou.py
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xchat
+
+# Copyright (c) 2014, Avail
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+__module_name__ = "Ascii -> Unicode"
+__module_version__ = "1.0"
+__module_description__ = "Converts normal text to fullwidth"
+
+WIDE_MAP = dict((i, i + 0xFEE0) for i in xrange(0x21, 0x7F))
+WIDE_MAP[0x20] = 0x3000
+ 
+def widen(s):
+    return unicode(s).translate(WIDE_MAP)
+
+def atou(word, word_eol, data):
+    xchat.command("say "+ widen(word_eol[1].decode('utf-8')).encode('utf-8'))
+    
+xchat.hook_command("atou",atou,help="/atou - Converts normal test to fullwidth")
